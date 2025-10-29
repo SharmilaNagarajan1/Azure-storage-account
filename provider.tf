219b4d03f1782d97c90352ca92c2c0dcc0fba476
sharmila <sharmeeh@gmail.com>
2025-10-30 00:26:30 +1100

    Initial commit

--- provider.tf
+++ provider.tf
@@ -1,9 +1,8 @@
 terraform {
-    required_version = ">=1.4.0"
-    required_providers {
-        azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 3.100"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.50.0"
     }
  }
 
